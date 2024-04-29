b4b1b8ac9dabb9790dd868e434817d81ea9ed1df
Jimmy Gong <jgong18@ucsc.edu>
2024-04-28 20:22:35 -0700

    drone testing updates

--- Drone Controls/CoordProcessing.py
+++ Drone Controls/CoordProcessing.py
@@ -9,11 +9,12 @@ import time
 import math
 from datetime import datetime
 
-TCP_IP = '128.114.51.113'
+# TCP_IP = '128.114.51.113'
 # TCP_IP = '169.233.221.226'
 # TCP_IP = '169.233.251.220'
 # TCP_IP = '169.233.192.251'
 # TCP_IP = '192.168.100.237'
+TCP_IP = '10.0.0.42'
 # TCP_IP = 'localhost'
 TCP_PORT = 5001
 class ProcessData:
@@ -38,7 +39,8 @@ class sCoord:
         self.x = x
         self.y = y
 
-tapDur = 500    #ms
+rotDur = 50    #ms
+horDur = 200    #ms
 # Galaxy S24, 2340x1080
 up = sCoord(473, 510)
 down = sCoord(473, 810)
@@ -50,9 +52,9 @@ left = sCoord(1760, 665)
 right = sCoord(2065, 665)
     
 horiThresh = 0.05    #meters
-vertThresh = 0.00    #meters
+vertThresh = 0.3    #meters
 rotThresh = 5       #degrees
-downtime = 20
+downtime = 100
 off_x = 0.0         #meters
 off_y = 0.0         #meters
 
@@ -186,61 +188,61 @@ while (True):
             PickleFile = pickle.loads(stringData)
         # print(PickleFile.data)
         #s.close()
-        pose = PickleFile.data
+        # pose = PickleFile.data
 
-        if (pose == None):
-            pose = [0, [[0], [0], [0]], [[0], [0], [0]], oldTime]
-            pose[1][0][0] = 0
-            pose[1][1][0] = 0
-            pose[1][2][0] = 0
-            pose[2][0][0] = 0
+        # if (pose == None):
+        #     pose = [0, [[0], [0], [0]], [[0], [0], [0]], oldTime]
+        #     pose[1][0][0] = 0
+        #     pose[1][1][0] = 0
+        #     pose[1][2][0] = 0
+        #     pose[2][0][0] = 0
 
-        # newCord[0]    # ID?    # Tag Family?
+        # # newCord[0]    # ID?    # Tag Family?
         # newCord[0] = pose[1][0][0] + off_x     # X in meters
-        newCord[0] = math.tan(pose[2][2][0]) * pose[1][2][0]    # X = tan(roll) * Z
+        # # newCord[0] = math.tan(pose[2][2][0]) * pose[1][2][0]    # X = tan(roll) * Z
         # newCord[1] = pose[1][1][0] + off_y     # Y in meters
-        newCord[1] = math.tan(pose[2][1][0]) * pose[1][2][0]    # Y = tan(pitch) * Z
-        newCord[2] = pose[1][2][0]      # Z in meters
-        newCord[3] = pose[2][0][0]      # Yaw in degrees
-        newCord[4] = pose[3]            # Time
+        # # newCord[1] = math.tan(pose[2][1][0]) * pose[1][2][0]    # Y = tan(pitch) * Z
+        # newCord[2] = pose[1][2][0]      # Z in meters
+        # newCord[3] = pose[2][0][0]      # Yaw in degrees
+        # newCord[4] = pose[3]            # Time
 
-    dir, dist = stateMach(newCord[0], newCord[1], newCord[2], newCord[3], newCord[4])
+    # dir, dist = stateMach(newCord[0], newCord[1], newCord[2], newCord[3], newCord[4])
     oldTime = newCord[4]
 
     #print(f"X: {newCord[0]}, Y: {newCord[1]}, Z: {newCord[2]}, Yaw: {newCord[3]}, Time: {newCord[4]}\r\n")
     if (dir != 'check'):
-        print(f"\r\nDirection: {dir}, Distance: {dist}")
+    #     # print(f"\r\nDirection: {dir}, Distance: {dist}")
         newT = int(datetime.now().strftime("%f"))
         elap = int(newT - prevT)
         # if (elap < 0):
             # elap =+ 1000000
         print("ms since Tag:", elap/1000, "\r\n")
         prevT = newT
-        cmd = ''
-        if (phone):
-            if (dir == 'up'):
-                cmd = "adb shell input swipe {0} {1} {0} {1} {2}".format(up.x, up.y, tapDur)
-            elif (dir == 'down'):
-                cmd = "adb shell input swipe {0} {1} {0} {1} {2}".format(down.x, down.y, tapDur)
-            elif (dir == 'left'):
-                cmd = "adb shell input swipe {0} {1} {0} {1} {2}".format(left.x, left.y, tapDur)
-            elif (dir == 'right'):
-                cmd = "adb shell input swipe {0} {1} {0} {1} {2}".format(right.x, right.y, tapDur)
-            elif (dir == 'forward'):
-                cmd = "adb shell input swipe {0} {1} {0} {1} {2}".format(forward.x, forward.y, tapDur)
-            elif (dir == 'backward'):
-                cmd = "adb shell input swipe {0} {1} {0} {1} {2}".format(backward.x, backward.y, tapDur)
-            elif (dir == 'ccw'):
-                cmd = "adb shell input swipe {0} {1} {0} {1} {2}".format(ccw.x, ccw.y, tapDur)
-            elif (dir == 'cw'):
-                cmd = "adb shell input swipe {0} {1} {0} {1} {2}".format(cw.x, cw.y, tapDur)
+    #     cmd = ''
+    #     if (phone):
+    #         if (dir == 'up'):
+    #             cmd = "adb shell input swipe {0} {1} {0} {1} {2}".format(up.x, up.y, horDur)
+    #         elif (dir == 'down'):
+    #             cmd = "adb shell input swipe {0} {1} {0} {1} {2}".format(down.x, down.y, horDur)
+    #         elif (dir == 'left'):
+    #             cmd = "adb shell input swipe {0} {1} {0} {1} {2}".format(left.x, left.y, horDur)
+    #         elif (dir == 'right'):
+    #             cmd = "adb shell input swipe {0} {1} {0} {1} {2}".format(right.x, right.y, horDur)
+    #         elif (dir == 'forward'):
+    #             cmd = "adb shell input swipe {0} {1} {0} {1} {2}".format(forward.x, forward.y, horDur)
+    #         elif (dir == 'backward'):
+    #             cmd = "adb shell input swipe {0} {1} {0} {1} {2}".format(backward.x, backward.y, horDur)
+    #         elif (dir == 'ccw'):
+    #             cmd = "adb shell input swipe {0} {1} {0} {1} {2}".format(ccw.x, ccw.y, rotDur)
+    #         elif (dir == 'cw'):
+    #             cmd = "adb shell input swipe {0} {1} {0} {1} {2}".format(cw.x, cw.y, rotDur)
             
-        if (cmd != ''):
-            os.system(cmd)
+    #     if (cmd != ''):
+    #         os.system(cmd)
 
-    if (phone):
-        if ((newCord[4] == 0) or (oldCase == 'stateStop')):
-            kill = "adb kill-server"
-            os.system(kill)
-            break
+    # if (phone):
+    #     if ((newCord[4] == 0) or (oldCase == 'stateStop')):
+    #         kill = "adb kill-server"
+    #         os.system(kill)
+    #         break
 
